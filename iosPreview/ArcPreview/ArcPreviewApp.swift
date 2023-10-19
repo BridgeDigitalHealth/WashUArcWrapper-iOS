deb9b4227ee088fd6acb4809fca38d4628c92aa8
Shannon Young <shannon.young@sagebase.org>
2023-10-19 12:40:45 -0700

    Add app for previewing icons

---
+++ iosPreview/ArcPreview/ArcPreviewApp.swift
@@ -0,0 +1,13 @@
+// Created 10/19/23
+// swift-tools-version:5.0
+
+import SwiftUI
+
+@main
+struct ArcPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
